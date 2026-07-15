5f33428d9ddbaa104f4745bff5d9fc08593ff758
Owen Summersett <osummersett@radianweapons.com>
2026-07-15 10:08:18 -0700

    Cleaned out .bat

--- kiosk_launcher.bat
+++ kiosk_launcher.bat
@@ -6,8 +6,6 @@ git fetch --all
 git reset --hard origin/main
 git clean -fd
 
-echo 1 > "..\1.txt"
-
 powershell -Command "Disable-PnpDevice -InstanceId 'HID\ELAN9038&COL01\5&145F55AC&0&0000' -Confirm:$false"
 taskkill /f /im ScreenClickTest.exe 2>nul
 taskkill /f /im msedge.exe 2>nul
