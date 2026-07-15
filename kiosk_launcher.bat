9b20c34a3e85170bcb62cd63ca8488ecbb8fa4b0
Owen Summersett <osummersett@radianweapons.com>
2026-07-15 10:26:31 -0700

    Added Kiosk number functionality

--- kiosk_launcher.bat
+++ kiosk_launcher.bat
@@ -9,6 +9,23 @@ git clean -fd
 powershell -Command "Disable-PnpDevice -InstanceId 'HID\ELAN9038&COL01\5&145F55AC&0&0000' -Confirm:$false"
 taskkill /f /im ScreenClickTest.exe 2>nul
 taskkill /f /im msedge.exe 2>nul
+
+REM Find kiosk number
+set "KIOSKNUM="
+
+for %%F in ("%USERPROFILE%\Desktop\?.txt") do (
+    if "%%~nF" GEQ "1" if "%%~nF" LEQ "7" (
+        set "KIOSKNUM=%%~nF"
+        goto :FoundKiosk
+    )
+)
+
+:FoundKiosk
+if defined KIOSKNUM (
+    powershell -Command ^
+        "(Get-Content 'C:\Users\Kiosk\Desktop\V2\config.ini') -replace '^Kiosk=.*','Kiosk=Kiosk%KIOSKNUM%' | Set-Content 'C:\Users\Kiosk\Desktop\V2\config.ini'"
+)
+
 start "" "C:\Users\Kiosk\Desktop\V2\kiosk_script.exe"
 start "" "msedge.exe" --kiosk "https://webtime2.paylocity.com/WebTime/Login/WebClock" --edge-kiosk-type=fullscreen
 timeout /t 30 /nobreak
