9ffcd2f7de73dd6a2f303c6d90bb61c4ef8eb62a
Owen Summersett <osummersett@radianweapons.com>
2026-07-15 10:43:53 -0700

    Made userprofile universal

--- kiosk_launcher.bat
+++ kiosk_launcher.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Update local V2 repository (force overwrite with GitHub version)
-cd /d "C:\Users\Kiosk\Desktop\V2"
+cd /d "C:\Users\%USERPROFILE%\Desktop\V2"
 git fetch --all
 git reset --hard origin/main
 git clean -fd
@@ -23,10 +23,10 @@ for %%F in ("%USERPROFILE%\Desktop\?.txt") do (
 :FoundKiosk
 if defined KIOSKNUM (
     powershell -Command ^
-        "(Get-Content 'C:\Users\Kiosk\Desktop\V2\config.ini') -replace '^Kiosk=.*','Kiosk=Kiosk%KIOSKNUM%' | Set-Content 'C:\Users\Kiosk\Desktop\V2\config.ini'"
+        "(Get-Content 'C:\Users\%USERPROFILE%\Desktop\V2\config.ini') -replace '^Kiosk=.*','Kiosk=Kiosk%KIOSKNUM%' | Set-Content 'C:\Users\%USERPROFILE%\Desktop\V2\config.ini'"
 )
 
-start "" "C:\Users\Kiosk\Desktop\V2\kiosk_script.exe"
+start "" "C\Users\%USERPROFILE%\Desktop\V2\kiosk_script.exe"
 start "" "msedge.exe" --kiosk "https://webtime2.paylocity.com/WebTime/Login/WebClock" --edge-kiosk-type=fullscreen
 timeout /t 30 /nobreak
 powershell -Command "Enable-PnpDevice -InstanceId 'HID\ELAN9038&COL01\5&145F55AC&0&0000' -Confirm:$false"
